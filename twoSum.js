013ebf9e864dfbab77f117e67ba2a28cb81f696d
Odewunmi Kofoworola <ojuolape05@gmail.com>
2025-02-26 08:04:58 +0100

    new change

--- twoSum.js
+++ twoSum.js
@@ -1,6 +1,6 @@
 var twoSum = function (nums, target) {
   for (let i = 0; i < nums.length; i++) {
-    for (let k = 0; k < nums.length; k++) {
+    for (let k = 1; k < nums.length; k++) {
       if (nums[i] + nums[k] == target) {
         return [i, k];
       }
@@ -8,7 +8,7 @@ var twoSum = function (nums, target) {
   }
   return [];
 };
-const nums = [11, 15, 2, 7];
+const nums = [8, 1, 2, 0];
 const target = 9;
 const result = twoSum(nums, target);
 console.log(result);
